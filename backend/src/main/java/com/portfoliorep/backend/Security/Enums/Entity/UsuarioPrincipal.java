5aed7be847337152bfcdb4295b4c9ffa2a37c6a0
zbmzk <kevinsalcedo@hotmail.com>
2022-08-22 03:52:22 -0300

    Update UsuarioPrincipal.java

--- backend/src/main/java/com/portfoliorep/backend/Security/Enums/Entity/UsuarioPrincipal.java
+++ backend/src/main/java/com/portfoliorep/backend/Security/Enums/Entity/UsuarioPrincipal.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.portfoliorep.backend.Security.Enums.Entity;
 
-/**
- *
- * @author Kevin
- */
-public class UsuarioPrincipal {
-    
+
+public class UsuarioPrincipal implements UserDetails{
+    private String nombre;
 }
